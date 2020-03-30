85d7ee97223d0cdb83d4c1ae68f8baa27d64ac9b
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-03-30 23:03:24 +0900

    Add tests

---
+++ testdir/10-length.lisp
@@ -0,0 +1,7 @@
+(print (length '(1 2 3 4 5)))
+(setq x '(a b c))
+(print (length (cdr x)))
+(print (length nil))
+(print (length ()))
+(print (length "Hello, World"))
+(print (length (cons 1 nil)))
---
+++ testdir/29-eval.lisp
@@ -0,0 +1,2 @@
+(print (eval '(+ 1 2)))
+(print (eval (cons '1+ (cons 1 nil))))
